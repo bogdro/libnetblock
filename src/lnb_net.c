2873d5dde40612b7db1d31c6dfa3c83abc3913ed
Bogdan Drozdowski <>
2023-11-19 23:15:06 +0100

    bindresvport: update comment

--- src/lnb_net.c
+++ src/lnb_net.c
@@ -285,7 +285,7 @@ bind (
 /* =============================================================== */
 
 #if (defined HAVE_BINDRESVPORT) \
-	&& (!defined __sun) /* skip on SunOS - different arguments, subject to changes */
+	&& (!defined __sun) /* skip on SunOS - different arguments, "kRPC internal function. Not for general use. Subject to rapid change." */
 
 # ifdef TEST_COMPILE
 #  undef LNB_ANSIC
